65542c646d711d83a44663a831283fc1e81cf63f
Nimafadaviklo <149236095+Nimafadaviklo@users.noreply.github.com>
2023-10-31 00:09:53 +0330

    Add files via upload

---
+++ فاکتوریلn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nفاکتوریل
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = 1, num;
+            Console.WriteLine("enter number");
+            num=Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                if (num == 0) break;
+                f *= i;
+            }
+            Console.WriteLine("{0}!= {1}",num,f);
+            Console.ReadKey();
+            //==========================================فاکتوریل n نیما فدوی 
+        }
+    }
+}
